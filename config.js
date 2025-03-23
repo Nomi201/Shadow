8b6a935e1971ae95724dc510b203c165f6c76dcb
Nomi201 <165730192+Nomi201@users.noreply.github.com>
2025-03-23 13:05:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || 'Raganork~c584fcbacc98c778e30d677c5b2d0cbd:32444a434a4558536b55493d',
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || 'Raganork~397f2e490fdbe666651f5c385727199d:4533737468524d52505a6b3d',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || 'Raganork;Skl11;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
